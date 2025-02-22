d8b3a73e20782972166e65140fb880bad34159bd
Bywren <Byronf91@proton.me>
2025-02-22 09:56:26 -0600

    Add files via upload

---
+++ docs/sketch.js
@@ -0,0 +1,21 @@
+const emojis = ["🤡", "😹", "💔", "😿"];
+const numbers = [1,2,3,4,5]
+
+function setup() {
+  createCanvas(800, 800);
+}
+{
+function draw() {
+  background(220);
+  frameRate(1)
+  for(number of numbers){}
+  let x = random(0, 700);
+  let y = random(0, 700);
+  textSize(75)
+  const emoji = random(emojis)
+  text(emoji,x,y)
+}
+
+ 
+ 
+  }
